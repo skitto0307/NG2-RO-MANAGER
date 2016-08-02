59e0702000193180b1535af8cee95ce3143f4b9b
skitto <skitto@swordfishweb.com>
2016-08-01 21:26:30 -0400

    config aspnetcore enviornmen to default to dev

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace NG2_RO_MANAGER
 {
@@ -11,11 +12,18 @@ namespace NG2_RO_MANAGER
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+              .AddEnvironmentVariables("")
+              .Build();
+
+            var env = config["ASPNETCORE_ENVIRONMENT"] ?? "Development";
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseEnvironment(env)
                 .Build();
 
             host.Run();
